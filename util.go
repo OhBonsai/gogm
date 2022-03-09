8da21998b3a57c042910925f071cefc44efa653a
OhBonsai <letbonsaibe@gmail.com>
2022-03-09 11:23:47 +0800

    fix: ignore some error

--- util.go
+++ util.go
@@ -331,10 +331,9 @@ func (r *relationConfigs) Validate() error {
 
 	for relType, validateConfig := range checkMap {
 		//check normal
-		//if len(validateConfig.Outgoing) != len(validateConfig.Incoming) {
-
-		//return fmt.Errorf("invalid directional configuration on relationship [%s], %w", relType, ErrValidation)
-		//}
+		if len(validateConfig.Outgoing) != len(validateConfig.Incoming) {
+			//return fmt.Errorf("invalid directional configuration on relationship [%s], %w", relType, ErrValidation)
+		}
 
 		//check both direction
 		if len(validateConfig.Both) != 0 {
